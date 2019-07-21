6a6c4ec0c08626cf2dc9e8eb8587f8791e12ea68
zhang_htao <1213462884@qq.com>
2019-07-21 23:02:38 +0800

    mybatis foreach标签的使用

--- src/main/java/com/mybatis/demo/controller/TeacherControll.java
+++ src/main/java/com/mybatis/demo/controller/TeacherControll.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController()
 public class TeacherControll {
     @Autowired
@@ -37,5 +42,42 @@ public class TeacherControll {
         Teacher teacher1 = dao.selectTeacherById(2);
         Teacher teacher2 = dao.selectTeacherById(4);
         return null;
+    }
+
+    @RequestMapping("/ListTeacherByConditons")
+    public Object ListTeacherByConditons() {
+        Map<String,Object> conditions = new HashMap<String,Object>();
+        conditions.put("sex", "1");
+        List<Teacher> teachers = dao.listTeacherByConditon(conditions);
+        return teachers;
+    }
+
+    @RequestMapping("/updateTeacherById")
+    public Object updateTeacherById() {
+        Teacher teacher = new Teacher();
+        teacher.setId(1);
+        teacher.setSex(2);
+        return dao.updateTeacherById(teacher) > 0;
+    }
+
+    @RequestMapping("/selectByIds")
+    public Object selectTeachersByIds() {
+        List<Teacher> teachers = dao.selectTeachersByIds(new Integer[]{1, 2, 4});
+        teachers.forEach(t->{
+            System.out.println( t);
+        });
+        return true;
+    }
+
+    @RequestMapping("/insertTeachers")
+    public Object insertTeachers() {
+        List<Teacher> teachers = new ArrayList<>();
+        Teacher teacher1 = new Teacher();
+        teacher1.setName("teacher1");
+        Teacher teacher2 = new Teacher();
+        teacher2.setName("teacher2");
+        teachers.add(teacher1);
+        teachers.add(teacher2);
+        return dao.insertTeachers(teachers);
     }
 }
--- src/main/java/com/mybatis/demo/mapper/TeacherMapper.java
+++ src/main/java/com/mybatis/demo/mapper/TeacherMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface TeacherMapper {
@@ -20,4 +21,14 @@ public interface TeacherMapper {
     List<Teacher> selectTeacherByDeptNum(@Param("deptNum") Integer deptNum);
 
     Teacher selectTeacherById(Integer id);
+
+
+    List<Teacher> listTeacherByConditon(Map<String, Object> conditions);
+
+    int updateTeacherById(Teacher teacher);
+
+    List<Teacher> selectTeachersByIds(@Param("ids") Integer[] ids);
+
+
+    int insertTeachers(@Param("teachers") List<Teacher> teachers);
 }
